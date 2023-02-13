b664fdd83ee619a9a2ca112a001af23d021dfd72
Darryl Estrada <darrylestrada97@gmail.com>
2023-02-13 21:35:15 +0100

    adding piecharts

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import { useList } from "@pankod/refine-core"
+
+import {
+    PieChart,
+    PropertyReferrals,
+    TotalRevenue,
+    PropertyCard,
+} from "components";
+import { Box, Typography, Stack } from '@pankod/refine-mui';
+
 
 const home = () => {
     return (
-        <div>home</div>
+        <Box>
+            <Typography fontSize={25} fontWeight={700} color="#11142D">
+                Dashboard
+                <Box mt="20px" display="flex" flexWrap="wrap" gap={4}>
+                    <PieChart title="Properties for Sale"
+                        value={684}
+                        series={[75, 25]}
+                        colors={["#475be8", "#e4e8ef"]} />
+                    <PieChart title="Properties for Rent"
+                        value={684}
+                        series={[60, 40]}
+                        colors={["#473ce8", "#e4e8ef"]} />
+                    <PieChart title="Total"
+                        value={5684}
+                        series={[75, 25]}
+                        colors={["#00A000", "#e4d8ef"]} />
+                    <PieChart title="Properties for Cities"
+                        value={555}
+                        series={[38, 62]}
+                        colors={["#A05be8", "#00A000"]} />
+                </Box>
+            </Typography>
+        </Box>
     )
 }
 
